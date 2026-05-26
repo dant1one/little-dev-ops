70181b2deb072bfab6a8a33353e04fc507d351c8
dant1one <vaahunovdaniyal@gmail.com>
2026-05-26 19:18:16 +0600

    Implemented log-archiver and changed comments in files

---
+++ scripts/log-archive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+'''
+This script is designed to archive log files in a specified directory. It compresses the log files
+'''
+
+clear
+
+LOG_DIR="/var/log"
+ARCHIVE_DIR="/var/log/myapp/archive"
--- scripts/server-stats.sh
+++ scripts/server-stats.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-'''
+: '
 Here u can see the system stats like CPU, Memory, Swap, Disk usage and top processes by CPU and Memory usage.
 This script is useful for monitoring the server performance and identifying any potential issues.
 main commands used in this script are:
@@ -9,7 +9,7 @@ main commands used in this script are:
 - ps: to get the top processes by CPU and Memory usage
 - awk: to format the output of the commands
 To run this script, save it to a file (e.g., server-stats.sh), give it execute permissions (chmod +x server-stats.sh), and then execute it (./server-stats.sh).
-'''
+'
 
 clear
 
